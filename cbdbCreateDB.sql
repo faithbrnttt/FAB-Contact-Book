5e70f5920376f4bd679b4867914675f47043cccf
Faith Burnett <110578486+faithbrnttt@users.noreply.github.com>
2023-09-21 02:05:04 -0500

    Delete cbdbCreateDB.sql

--- cbdbCreateDB.sql
+++
@@ -1,9 +0,0 @@
-use cbdb;
-
-CREATE TABLE contacts (
-	contactID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    contactName VARCHAR(100) NOT NULL,
-    contactPhone VARCHAR(100) NOT NULL
-);
-
-SELECT * FROM contacts;
